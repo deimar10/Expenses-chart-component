0b8c0917d488224aecd6c1e471c1df71e8feb249
Deimar <deimarparn14@gmail.com>
2022-10-12 15:35:40 +0300

    function changes the chart color on current weekday

--- assets/main.js
+++ assets/main.js
@@ -1,44 +1,26 @@
 var xValues = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var yValues = [17.45, 34.91, 52.36, 31.07, 23.39, 42.28, 25.48];
 const backgroundcolor = [];
+var a = new Date();
+var weekdays = new Array(7);
+    weekdays[0] = "Sunday";
+    weekdays[1] = "Monday";
+    weekdays[2] = "Tuesday";
+    weekdays[3] = "Wednesday";
+    weekdays[4] = "Thursday";
+    weekdays[5] = "Friday";
+    weekdays[6] = "Saturday";
+    var r = weekdays[a.getDay()];
+    console.log(r);
 
 for(i = 0; i < xValues.length; i++) {
-    if(xValues[i] === "Monday") { 
+    if(r === xValues[i]) { 
         backgroundcolor.push('hsl(186, 34%, 60%)') 
     } else {
         backgroundcolor.push("hsl(10, 79%, 65%)") 
     }
-    if(xValues[i] === "Tuesday") {
-         backgroundcolor.push( "hsl(186, 34%, 60%)") 
-    } else {
-        backgroundcolor.push("hsl(10, 79%, 65%)") 
-    }
-    if(xValues[i] === "Wednesday") {
-         backgroundcolor.push( "hsl(186, 34%, 60%)") 
-        } else {
-        backgroundcolor.push("hsl(10, 79%, 65%)") 
-    }
-    if(xValues[i] === "Thursday") {
-        backgroundcolor.push( "hsl(186, 34%, 60%)") 
-        } else {
-        backgroundcolor.push("hsl(10, 79%, 65%)") 
-    }
-    if(xValues[i] === "Friday") {
-        backgroundcolor.push( "hsl(186, 34%, 60%)") 
-        } else {
-        backgroundcolor.push("hsl(10, 79%, 65%)") 
-    }
-    if(xValues[i] === "Saturday") {
-        backgroundcolor.push( "hsl(186, 34%, 60%)") 
-        } else {
-        backgroundcolor.push("hsl(10, 79%, 65%)") 
-    }
-    if(xValues[i] === "Sudnay") {
-        backgroundcolor.push( "hsl(186, 34%, 60%)") 
-        } else {
-        backgroundcolor.push("hsl(10, 79%, 65%)") 
-    }
 }
+
 
 new Chart("myChart", {
     type: "bar",
